18bd079be2af73e7f21eabaef553abd8effdd39d
gomama <maji.gomama@gmail.com>
2018-01-10 22:19:27 +0900

    complete implements blockchain class

--- blockchain.rb
+++ blockchain.rb
@@ -11,12 +11,11 @@ class BlockChain
   end
 
   def new_block(proof, previous_hash=nil)
-=begin
-    Create a new Block in the Blockchain
-    :param proof: <int> The proof given by the Proof of Work algorithm
-    :param previous_hash: (Optional) <str> Hash of previous Block
-    :return: <dict> New Block
-=end
+
+    # Create a new Block in the Blockchain
+    # :param proof: <int> The proof given by the Proof of Work algorithm
+    # :param previous_hash: (Optional) <str> Hash of previous Block
+    # :return: <dict> New Block
 
     block = {
       index: @chain.length + 1,
@@ -34,14 +33,12 @@ class BlockChain
   end
 
   def new_transaction(sender, recipient, amount)
-=begin
-    Creates a new transaction to go into the next mined Block
+    # Creates a new transaction to go into the next mined Block
       
-    :param sender: <str> Address of the Sender
-    :param recipient: <str> Address of the Recipient
-    :param amount: <int> Amount
-    :return: <int> The index of the Block that will hold this transaction
-=end
+    # :param sender: <str> Address of the Sender
+    # :param recipient: <str> Address of the Recipient
+    # :param amount: <int> Amount
+    # :return: <int> The index of the Block that will hold this transaction
 
     @current_transactions << {
       sender: sender,
@@ -52,20 +49,37 @@ class BlockChain
     return @current_transactions.last_block[:index] + 1
   end
 
+  def proof_of_work(last_proof)
+     #  Simple Proof of Work Algorithm:
+     #    - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+     #    - p is the previous proof, and p' is the new proof
+     #  :param last_proof: <int>
+     #  :return: <int>
+
+    proof = 0
+    while !valid_proof(last_proof, proof) { proof += 1}
+
+    return proof
+  end
+
   def last_block
     # return last_block
     return @chain.last
   end
 
   def self.hash(block)
-=begin
-    Creates a SHA-256 hash of a Block
-    :param block: <dict> Block
-    :return: <str>
-=end
+    # Creates a SHA-256 hash of a Block
+    # :param block: <dict> Block
+    # :return: <str>
 
     # We must make sure that the Dictionary is ordered, or we'll have inconsistent hashes
     block_string = JSON.generate(@chain.map(&:sort))
     return Digest::SHA256.hexdigest(block_string)
+  end
+
+  def self.valid_proof(last_proof, proof)
+    guess = "#{last_proof}#{proof}"
+    guess_hash = Digest::SHA256.hexdigest(guess)
+    return guess_hash[0,4] == "0000"
   end
 end
